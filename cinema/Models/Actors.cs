ff36e6112d5e43384c88445ccfb95041ef55db8f
gamerLlife <53333997+gamerLlife@users.noreply.github.com>
2023-03-22 20:22:49 +0800

    chill

---
+++ cinema/Models/Actors.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cinema.Models
+{
+    public class Actors
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+
+        public string Fullname { get; set;}
+
+        public string Bio { get; set; }
+    }
+}
